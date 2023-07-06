5bec7919d68c142f163d86bba2c9e27fc9896ff7
Samir Dilly <samir9.dilly@gmail.com>
2023-07-06 13:38:42 -0300

    add use request

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from "./modules/home/screen/Home";
 import TestLogin from "./modules/testLogin/TestLogin";
+import store from './store'
+import { Provider } from 'react-redux'
 
 const App = () => {
 
   const Stack = createNativeStackNavigator();
     return(
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="TestLogin" component={TestLogin} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="TestLogin" component={TestLogin} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
     )
 }
 
--- src/shared/hooks/useRequest.ts
+++ src/shared/hooks/useRequest.ts
@@ -1,21 +1,22 @@
 import { useState } from "react"
 import { RequestLogin } from "../types/requestLogin";
-import axios from "axios";
+
 import { connectionAPIPost } from "../functions/connection/connectionAPI";
-import { ReturnLogin } from "../types/returnLogin";
+
+import { useNavigation } from "@react-navigation/native";
+import { setAuthorizationToken } from "../functions/connection/auth";
 
 
 
 export const useRequest = () => {
-
-
     const [errorMessage, setErrorMessage] = useState<string>('');
-    const [user, setUser] = useState<string>('');
-
+    const navigation = useNavigation();
     const authRequest = async (body: RequestLogin) => {
-        await connectionAPIPost<ReturnLogin>('http://192.168.254.165/apijwt/login', body)
+        await connectionAPIPost('http://192.168.254.165/apijwt/login', body)
         .then((result) => {
-            console.log(result.acessToken)
+            setAuthorizationToken(result)
+            console.log(result)
+            navigation.navigate("Home")
         })
         .catch(() => {
             setErrorMessage('Usuário ou Senha inválidos');
